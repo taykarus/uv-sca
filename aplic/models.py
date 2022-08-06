2bd0eb8cc66d7c463b09f6f68810f3d0b3e65064
Alison Israel <taykarus@gmail.com>
2022-08-06 18:55:50 -0300

    feat: adiciona model de cursos

--- aplic/models.py
+++ aplic/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Curso(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    descricao = models.TextField('Descrição', max_length=500)
+    carga_horaria = models.IntegerField('Carga Horária')
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
+    def __str__(self):
+        return self.nome
