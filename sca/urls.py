94377ae25be682660e683473f40c6d004c456f35
Alison Israel <taykarus@gmail.com>
2022-08-07 15:05:32 -0300

    ops: adiciona configs para static e media files no heroku

--- sca/urls.py
+++ sca/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('restrito/', admin.site.urls),
     path('', include('aplic.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
