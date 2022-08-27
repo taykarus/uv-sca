cacdc1940294bf2e404788f7e1466e45e2e6bb62
Alison Israel <taykarus@gmail.com>
2022-08-27 11:44:27 -0300

    feat: adiciona botoes dinamicos

--- aplic/views.py
+++ aplic/views.py
@@ -4,6 +4,16 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['menu_active_item'] = 'nav-item-home'
+        return context
+
 
 class SobreView(TemplateView):
     template_name = 'about-us.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SobreView, self).get_context_data(**kwargs)
+        context['menu_active_item'] = 'nav-item-about_us'
+        return context
