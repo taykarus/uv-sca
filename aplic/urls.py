16a34aff93f011491a6a5721758bc3b44469ce45
Alison Israel <taykarus@gmail.com>
2022-09-03 11:32:05 -0300

    feat: adiciona relatório de alunos

--- aplic/urls.py
+++ aplic/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import IndexView, AboutView, ProfessorListView, CursoDetailView, AlunosChartView
+from .views import IndexView, AboutView, ProfessorListView, CursoDetailView, AlunosChartView, RelatorioAlunosView
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('professores/', ProfessorListView.as_view(), name='professor-list'),
     path('cursos/<int:pk>/', CursoDetailView.as_view(), name='curso-detail'),
     path('alunos/grafico/', AlunosChartView.as_view(), name='alunos-grafico'),
+    path('relatorio-alunos/', RelatorioAlunosView.as_view(), name='relatorio-alunos'),
 ]
